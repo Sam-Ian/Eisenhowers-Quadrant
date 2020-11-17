053ebe81cd0fa9e396ec7e1e2bb07bc33a3f5884
Sam Thompson <samthompson292@gmail.com>
2020-11-17 11:46:07 +0000

    added lists

--- quadrant.py
+++ quadrant.py
@@ -1,5 +1,12 @@
 from prettytable import prettytable
 
+urgent_important = [] 
+non_urgent_important = []
+urgent_non_important = []
+non_urgent_non_important = []
+
+
+
 t = PrettyTable(['', 'Urgent', 'Non-Urgent'])
 t.add_row(['Important', urgent_important, non_urgent_important])
 t.add_row(['Non-Important', urgent_non_important, non_urgent_non_important])
