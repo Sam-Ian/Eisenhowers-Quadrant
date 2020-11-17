c965fb9f9cbf2e098cabcdf15c77c4243a78848a
Sam Thompson <samthompson292@gmail.com>
2020-11-17 12:38:43 +0000

    reads from csv file and adds to table in correct format

--- quadrant.py
+++ quadrant.py
@@ -1,17 +1,72 @@
-from prettytable import prettytable
+from prettytable import PrettyTable, ALL
+import csv
 
-urgent_important = [] 
-non_urgent_important = []
-urgent_non_important = []
-non_urgent_non_important = []
+##############################################################################
+
+#IMPORTING CSV DATA
+
+with open('Eisenhower.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    urgent_important = []
+    non_urgent_important = []
+    urgent_non_important = []
+    non_urgent_non_important = []
+
+    for task in csv_reader:
+        if task['Urgent/Non Urgent'] == 'Urgent' and task['Important/Non Important'] == 'Important':
+            urgent_important.append(task['Task'])
+        elif task['Urgent/Non Urgent'] == 'Non Urgent' and task['Important/Non Important'] == 'Important':
+            non_urgent_important.append(task['Task'])
+        elif task['Urgent/Non Urgent'] == 'Urgent' and task['Important/Non Important'] == 'Non Important':
+            urgent_non_important.append(task['Task'])
+        elif task['Urgent/Non Urgent'] == 'Non Urgent' and task['Important/Non Important'] == 'Non Important':
+            non_urgent_non_important.append(task['Task'])
+        else:
+            print('Error, please define urgency and importance.')
 
 
+formatted_urgent_important = '\n'.join(urgent_important)
+formatted_non_urgent_important = '\n'.join(non_urgent_important)
+formatted_urgent_non_important = '\n'.join(urgent_non_important)
+formatted_non_urgent_non_important = '\n'.join(non_urgent_non_important)
 
-t = PrettyTable(['', 'Urgent', 'Non-Urgent'])
-t.add_row(['Important', urgent_important, non_urgent_important])
-t.add_row(['Non-Important', urgent_non_important, non_urgent_non_important])
 
+##############################################################################
+
+#CREATING THE TABLE
+
+
+t = PrettyTable()
+t.hrules = ALL
+t.field_names = ['', 'Urgent', 'Non-Urgent']
+t.add_row(['Important', formatted_urgent_important, formatted_non_urgent_important])
+t.add_row(['Non-Important', formatted_urgent_non_important, formatted_non_urgent_non_important])
 print(t)
+
+
+##############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
