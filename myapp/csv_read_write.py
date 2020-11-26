89ff8b83cbe9f4b57c3f3d1f3a747848b5948a30
Sam Thompson <samthompson292@gmail.com>
2020-11-26 17:24:58 +0000

    fixed VE on windpws

--- myapp/csv_read_write.py
+++ myapp/csv_read_write.py
@@ -10,15 +10,15 @@ with open('Eisenhower.csv') as csv_file_read:
         task_name[task['Task ID']] = (task['Task'])
         task_type[task['Task ID']] = (task['Urgent/Important'])
 
-with open('Eisenhower_test.csv') as csv_file_write:
-    fields = ['Task ID', 'Task', 'Urgent/Important']
-    csv_writer = csv.DictWriter(csv_file_write, fieldnames=fields)
+# with open('Eisenhower_test.csv') as csv_file_write:
+#     fields = ['Task ID', 'Task', 'Urgent/Important']
+#     csv_writer = csv.DictWriter(csv_file_write, fieldnames=fields)
 
-    csv_writer.writeheader()
+#     csv_writer.writeheader()
 
-    for id, name in task_name.items():
-        csv_writer.writerow(name)
-    for id, t_type in task_type.items():
-        csv_writer.writerow(t_type)
+#     # for id, name in task_name.items():
+#     #     csv_writer.writerow(name)
+#     # for id, t_type in task_type.items():
+#     #     csv_writer.writerow(t_type)
 
 
