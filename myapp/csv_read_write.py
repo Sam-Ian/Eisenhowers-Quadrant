a6a2bd9c07a6e487372c184b8806c6778ff9e425
Sam Thompson <samthompson292@gmail.com>
2020-11-26 20:07:14 +0000

    add table to view tasks

--- myapp/csv_read_write.py
+++ myapp/csv_read_write.py
@@ -8,3 +8,4 @@ with open('Eisenhower.csv') as csv_file_read:
     for task in csv_reader:
         task_name[task['Task ID']] = (task['Task'])
         task_type[task['Task ID']] = (task['Urgent/Important'])
+
