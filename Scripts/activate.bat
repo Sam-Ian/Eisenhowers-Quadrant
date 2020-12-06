9fd3f376bb2656ecfc6fa2cd691a9f224820fae5
Sam Thompson <samthompson292@gmail.com>
2020-12-06 17:56:58 +0000

    redo pip freeze requirements.txt

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Sam\Documents\GitHub\eisenhowers_quadrant\eisenhowers_quadrant
+set VIRTUAL_ENV=C:\Users\Sam\Documents\GitHub\eisenhowers_quadrant
 
 if not defined PROMPT set PROMPT=$P$G
 
